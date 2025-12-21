2be15951c20fa209dc2630a545109e15411bc13f
AmirHossein137 <122604637+AmirHossein137@users.noreply.github.com>
2025-12-21 14:23:40 +0330

    add text

--- src/components/modules/Cart.jsx
+++ src/components/modules/Cart.jsx
@@ -17,43 +17,43 @@ const Cart = ({ fooods }) => {
     <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
       {fooods.map((item) => (
         <div
-  key={item.id}
-  className="group flex flex-col bg-white p-3 rounded-xl shadow transition-all duration-200 hover:-translate-y-3"
->
-  <div className="bg-[#f2f2f2] w-full h-[286px] rounded-xl flex items-center justify-center mb-4 overflow-hidden">
-    <Image
-      src={item.image}
-      width={220}
-      height={220}
-      alt={item.title}
-      className="rounded-md object-cover transition-transform duration-300 group-hover:scale-110"
-    />
-  </div>
+          key={item.id}
+          className="group flex flex-col bg-white p-3 rounded-xl shadow transition-all duration-200 hover:-translate-y-3"
+        >
+          <div className="bg-[#f2f2f2] w-full h-[286px] rounded-xl flex items-center justify-center mb-4 overflow-hidden">
+            <Image
+              src={item.image}
+              width={220}
+              height={220}
+              alt={item.title}
+              className="rounded-md object-cover transition-transform duration-300 group-hover:scale-110"
+            />
+          </div>
 
-  <span className="block text-center font-bold">{item.title}</span>
+          <span className="block text-center font-bold">{item.title}</span>
 
-  <div className="flex items-center justify-center gap-2">
-    <div className="flex items-center gap-1">
-      <span>{numberFormat(item.minPrice)}</span>
-      <span className="text-sm">تومان</span>
-    </div>
-    <span>-</span>
-    <div className="flex items-center gap-1">
-      <span>{numberFormat(item.maxprice)}</span>
-      <span className="text-sm">تومان</span>
-    </div>
-  </div>
-
-  <div className="flex items-center justify-center">
+          <div className="flex items-center justify-center gap-2">
+            <div className="flex items-center gap-1">
+              <span>{numberFormat(item.minPrice)}</span>
+              <span className="text-sm">تومان</span>
+            </div>
+            <span>-</span>
+            <div className="flex items-center gap-1">
+              <span>{numberFormat(item.maxprice)}</span>
+              <span className="text-sm">تومان</span>
+            </div>
+          </div>
+          <p className="text-sm text-justify text-gray-600">{item.text}</p>
+          {/* <div className="flex items-center justify-center">
     <ReactStars
       value={item.rate}
       size={20}
       color2="#facc15"
       edit={false}
     />
-  </div>
+  </div> */}
 
-  <div className="flex items-center justify-center gap-3 mt-4">
+          {/* <div className="flex items-center justify-center gap-3 mt-4">
     <button className="w-9 h-9 flex items-center justify-center border border-[#ddd] rounded">
       <FaRegEye />
     </button>
@@ -63,9 +63,8 @@ const Cart = ({ fooods }) => {
     <button className="w-9 h-9 flex items-center justify-center border border-[#ddd] rounded">
       <MdFavorite />
     </button>
-  </div>
-</div>
-
+  </div> */}
+        </div>
       ))}
     </div>
   );
